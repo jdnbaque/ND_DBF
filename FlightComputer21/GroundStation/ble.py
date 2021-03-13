58144aa41ba5328e4148b5fd162ec1502585d173
Ariel <jbaque@nd.edu>
2021-03-13 15:57:13 -0500

    Ground station added

---
+++ FlightComputer21/GroundStation/ble.py
@@ -0,0 +1,22 @@
+import socket
+
+adapter_addr = 'e4:a4:71:63:e1:69'
+port = 3  # Normal port for rfcomm?
+buf_size = 1024
+
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.bind((adapter_addr, port))
+s.listen(1)
+try:
+    print('Listening for connection...')
+    client, address = s.accept()
+    print(f'Connected to {address}')
+
+    while True:
+        data = client.recv(buf_size)
+        if data:
+            print(data)
+except Exception as e:
+    print(f'Something went wrong: {e}')
+    client.close()
+    s.close()

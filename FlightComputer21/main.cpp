b4f1586f68e5b0d3f06c810dee4c765223570445
Ariel <jbaque@nd.edu>
2021-03-14 00:00:02 -0500

    Ground station added

--- FlightComputer21/main.cpp
+++ FlightComputer21/main.cpp
@@ -4,25 +4,30 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM6DSOX.h>
 #include "Adafruit_BMP3XX.h"
+#include <string.h>
 
-uint8_t uvindexvalue = 0x0;
+uint32_t uvindexvalue;
 #define UUID16_SVC_ENVIRONMENTAL_SENSING 0X181A
-#define UUID_CHR_UV_INDEX 0X2A76
+#define UUID16_SVC_LOCATION_AND_NAVIGATION 0X1819
+
+#define UUID_CHR_UV_INDEX 0X2A6E
+#define UUID_CHR_TEMPERATURE 0x2A6E 
+
 #define SEALEVELPRESSURE_HPA (1013.25)
 Adafruit_LSM6DSOX sox; 
 Adafruit_BMP3XX bmp;
 
 
 BLEService environmental_sensing_service = BLEService(UUID16_SVC_ENVIRONMENTAL_SENSING);
-BLECharacteristic uv_index_characteristic = BLECharacteristic(UUID_CHR_UV_INDEX);
+BLECharacteristic uv_index_characteristic = BLECharacteristic(UUID_CHR_TEMPERATURE);
 
 void setupESService(void){
   environmental_sensing_service.begin();
   uv_index_characteristic.setProperties(CHR_PROPS_INDICATE);
   uv_index_characteristic.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
-  uv_index_characteristic.setFixedLen(0);
+  uv_index_characteristic.setFixedLen(4);
   uv_index_characteristic.begin();
-  uv_index_characteristic.write(&uvindexvalue, sizeof(uvindexvalue));
+  uv_index_characteristic.write(&uvindexvalue, 4);
 }
 
 void startAdv(void) {
@@ -32,7 +37,7 @@ void startAdv(void) {
   Bluefruit.Advertising.restartOnDisconnect(true);
   Bluefruit.Advertising.setInterval(32, 244);
   Bluefruit.Advertising.setFastTimeout(30);
-  Bluefruit.Advertising.start(0);
+  Bluefruit.Advertising.start();
 }
 
 void setup() {
@@ -198,13 +203,15 @@ void loop() {
   sensors_event_t gyro;
   sensors_event_t temp;
   sox.getEvent(&accel, &gyro, &temp);
-  uvindexvalue = temp.temperature;
+  uvindexvalue = temp.temperature*100.0;
+  Serial.println(sizeof(uvindexvalue));
   if(uv_index_characteristic.indicate(&uvindexvalue, sizeof(uvindexvalue))){
-    Serial.print("Updated Temp: ");
-    Serial.print('\t' + uvindexvalue + '\n');
+    Serial.print("Updated Temp: " );
+    Serial.println(uvindexvalue);
   }else {
-    Serial.println("error, no receivers");
+    Serial.println("error, no receivers or no message sent");
   }
-  delay(500);
+  delay(1000);
 }
+
 
